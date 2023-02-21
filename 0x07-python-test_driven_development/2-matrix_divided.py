ff812042e7f870c0699683b0d981842e22b9b02b
kenekanem <kennedyekanem@gmail.com>
2023-02-21 09:18:55 +0100

    3-say_my_name.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,26 +1,44 @@
 #!/usr/bin/python3
 """
-Divisible all elements of a matrix.
+Function that divides elements of a matrix
 """
-def matrix_divided(matrix, div):
-    """Divides all elements of a matrix by a given number.
-        matrix(list) is divided by the div(float/int)
-    Returns: a new matrix list
-    """
-    if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    if not isinstance(div, (int, float)):
-        raise TypeError("div must be a number")
-    if div == 0:
-        raise ZeroDivisionError:("division by zero")
 
-    for row in matrix:
-        if not isinstance(row, list):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-        first_row = len(matrix[0])
-        if len(row) != first_row:
-            raise TypeError("Each row of the matrix must have the same size")
-        for items in row:
-            if not isinstance(items,(int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    return [list(map(lambda item: round((items / div), 2), row)) for row in matrix]
+
+def matrix_divided(matrix, div):
+    """
+    Divides all elements of a matrix
+    Arguments:
+    matrix: must be a list of lists ints or floats
+    with rows of the same size
+    div: must be a number int or float and not 0
+    Result is rounded to 2 decimal places
+    and a new matrix is generated
+    Raise TypeError or ZeroDivisionError if conditions not
+    met
+    """
+
+    if not matrix or \
+       not isinstance(matrix, list) or \
+       not all(isinstance(i, list) for i in matrix) or \
+       not all(len(j) for j in matrix) or \
+       not all([all(isinstance(k, (int, float)) for k in m) for m in matrix]):
+
+        msg = "matrix must be a matrix (list of lists) of integers/floats"
+        raise TypeError(msg)
+
+    checkl = [len(r) for r in matrix]
+
+    if len(set(checkl)) != 1:
+        msg = "Each row of the matrix must have the same size"
+        raise TypeError(msg)
+
+    if not isinstance(div, (int, float)) or div != div:
+        msg = "div must be a number"
+        raise TypeError(msg)
+
+    if div == 0:
+        msg = "division by zero"
+        raise ZeroDivisionError(msg)
+
+    new_m = [[round(j / div, 2) for j in i] for i in matrix]
+    return (new_m)
--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python3
-#prints user firstname and lastname
+"""
+This module is composed by a function prints a message
+"""
+
 
 def say_my_name(first_name, last_name=""):
-    """Print firstname appended with lastname, raises error is name is not a string"""
+    """ Function that prints "My name is <first name> <last name>"
+    Args:
+        first_name: first name
+        last_name: last name
+    Returns:
+        No return
+    Raises:
+        TypeError: If first_name or last_name is not a string
+    """
+
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
 
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
 
-    print("My name is {:s} {:s}".format(first_name, last_name))
+    print("My name is {} {}".format(first_name, last_name))
