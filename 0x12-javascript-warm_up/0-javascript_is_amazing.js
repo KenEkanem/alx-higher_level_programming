6ad013e7e425d35746d625227521ede320dddea6
kenekanem <kennedyekanem@gmail.com>
2023-04-14 12:55:52 +0100

    removed old dir

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 
-// Script that prints "JavaScript is amazing"
+//script that prints 'Javascript is amazing'
 
-const myVar = 'JavaScript is amazing';
-console.log(myVar);
+const myVar = 'Javascript is amazing'
