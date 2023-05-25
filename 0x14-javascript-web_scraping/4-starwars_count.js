69e01f0e193081de647e20683cd61d38a4e5ffbf
Kennedy Ekanem <kennedyekanem@gmail.com>
2023-05-25 04:04:09 +0100

    update movie count

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,15 +1,13 @@
 #!/usr/bin/node
 // A script that prints the number of movies where the character “Wedge Antilles” is present
 const request = require('request');
-const url = process.argv[2];
-
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const films = JSON.parse(body).results;
-    const wedgeMovies = films.filter((film) =>
-      film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/'));
-    console.log(wedgeMovies.length);
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
