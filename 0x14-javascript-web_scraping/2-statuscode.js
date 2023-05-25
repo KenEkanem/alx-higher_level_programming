ec6b9ac059d1340239e8951f512c47d46b888481
Kennedy Ekanem <kennedyekanem@gmail.com>
2023-05-25 03:44:44 +0100

    get request stats

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// A script that display the status code of a GET request.
+const request = require('request');
+const url = process.argv[2];
+request
+  .get(url)
+  .on('response', function (response) {
+    console.log(`code: ${response.statusCode}`);
+  });
